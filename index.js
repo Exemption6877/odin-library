7cb24014e70641b11e845446648af756b0da2aaf
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-08 00:56:00 +0300

    Add input fetch functionality

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-function addBookToLibrary(title, author, pages, status) {
-  const entry = new Book(title, author, pages, status);
+function addBookToLibrary(entry) {
   myLibrary.push(entry);
+  console.log(myLibrary);
 }
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const status = document.querySelector("#status").value;
+
+  const entry = new Book(title, author, pages, status);
+  addBookToLibrary(entry);
+});
+
+// Dialog
 
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#show-dialog");
