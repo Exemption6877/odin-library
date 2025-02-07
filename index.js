3ba107f48c68e92dc9c81c39ca29beaa63c063ff
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-08 00:43:25 +0300

    Add placeholder entry, dialog & some CSS changes.

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function addBookToLibrary(title, author, pages, status) {
   const entry = new Book(title, author, pages, status);
   myLibrary.push(entry);
 }
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#show-dialog");
+const closeButton = document.querySelector("#close-dialog");
+
+showButton.addEventListener("click", () => {
+  dialog.show();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
